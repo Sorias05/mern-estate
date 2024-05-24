137df6df23fd193a1897746d4f5626849b14d07f
Sorias05 <sashko2b3@gmail.com>
2024-05-24 18:48:25 +0300

    complete search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
+import ListingComponent from "../components/ListingComponent";
+import { useToast } from "../redux/ToastProvider";
 
 export default function Search() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [listings, setListings] = useState([])
+  const [showMore, setShowMore] = useState(false);
+  const [listings, setListings] = useState([]);
+  const { showToast } = useToast();
   const [sidebarData, setSidebarData] = useState({
     searchTerm: '',
     type: 'all',
@@ -14,9 +18,6 @@ export default function Search() {
     sort: 'created_at',
     order: 'desc',
   });
-
-  console.log(sidebarData);
-  console.log(listings);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -53,8 +54,16 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if (data.length >= 9)
+        setShowMore(true); 
+      else
+        setShowMore(false);
       setListings(data);
       setLoading(false);
+      if (data.length > 0)
+        showToast(true, "Listings found successfully");
+      else 
+        showToast(false, "Listings not found");
     }
 
     fetchListings();
@@ -96,10 +105,24 @@ export default function Search() {
     navigate(`/search?${searchQuery}`);
   };
 
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res  = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data])
+  }
+
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="flex flex-col max-w-screen-2xl mx-auto md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8 top-[100px] sticky">
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">Search Term:</label>
             <input
@@ -153,8 +176,24 @@ export default function Search() {
           <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">Search</button>
         </form>
       </div>
-      <div className="">
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">Listing results:</h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
+          )}
+          {!loading && listings && listings.map((listing) => (
+            <ListingComponent key={listing._id} listing={listing} />
+          ))}
+          {showMore && (
+            <button onClick={onShowMoreClick} className="text-green-700 hover:underline p-7 text-center w-full">
+              Show more
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
